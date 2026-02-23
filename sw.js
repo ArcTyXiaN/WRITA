7a1d6f97030823877f588c048ef25d4633c03bf3
ArcTyXiaN <obaloluwao008@gmail.com>
2026-02-23 01:24:03 +0100

    adding files

---
+++ sw.js
@@ -0,0 +1,41 @@
+﻿const CACHE_NAME = 'writa-v2';
+const OFFLINE_URL = './index.html';
+const ASSETS = [
+  './',
+  './index.html',
+  './WRITA.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request).catch(() => caches.match(OFFLINE_URL))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => cached || fetch(request))
+  );
+});
